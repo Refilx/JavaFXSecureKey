3865ee9ce87a143c67f36d9d849e3be84f07ad8f
Refilx <jovenestudantex11@gmail.com>
2025-04-17 09:39:08 -0300

    Implementação da tela de FXMLEmprestarChaveScreen.fxml e da classe controladora básica FXMLEmprestarChaveController.java

---
+++ src/main/java/br/com/javafxsecurekey/controller/FXMLEmprestarChaveController.java
@@ -0,0 +1,69 @@
+package br.com.javafxsecurekey.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class FXMLEmprestarChaveController {
+
+    @FXML
+    private Button btnCadastrar;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Pane cadastroUserScreen;
+
+    @FXML
+    private ListView<?> lvNumChave;
+
+    @FXML
+    private ListView<?> lvSolicitante;
+
+    @FXML
+    private ScrollPane scrollPaneNumeroChave;
+
+    @FXML
+    private ScrollPane scrollPaneSolicitante;
+
+    @FXML
+    private TextArea taDescricao;
+
+    @FXML
+    private TextField tfNumChave;
+
+    @FXML
+    private TextField tfSolicitante;
+
+    @FXML
+    void btnCadastrarMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnCancelarMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void taDescricaoOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void tfNumChaveOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void tfSolicitanteOnMouseClicked(MouseEvent event) {
+
+    }
+
+}
