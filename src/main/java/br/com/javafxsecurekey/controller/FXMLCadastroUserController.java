309653c3ed7bf39f05f5002001c10f37d4abad8b
Refilx <jovenestudantex11@gmail.com>
2025-04-18 14:55:45 -0300

    Modificação profunda da classe FXMLCadastroUserController com a exclusão de uma série de verificações e dados desnecessários + inclusão do carregamento das pessoas cadastradas no banco de dados na ListView

--- src/main/java/br/com/javafxsecurekey/controller/FXMLCadastroUserController.java
+++ src/main/java/br/com/javafxsecurekey/controller/FXMLCadastroUserController.java
@@ -3,21 +3,21 @@ package br.com.javafxsecurekey.controller;
 import br.com.javafxsecurekey.model.dao.PessoaDAO;
 import br.com.javafxsecurekey.model.dao.UsuarioDAO;
 import br.com.javafxsecurekey.model.dao.VerifyDAO;
+import br.com.javafxsecurekey.model.domain.Pessoa;
 import br.com.javafxsecurekey.model.domain.Usuario;
 import br.com.javafxsecurekey.model.util.CaseTextFormatter;
-import br.com.javafxsecurekey.model.util.TextFieldFormatter;
-import br.com.javafxsecurekey.model.validator.CPFValidator;
-import br.com.javafxsecurekey.model.validator.EmailValidator;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 import javax.swing.*;
 import java.net.URL;
-import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 public class FXMLCadastroUserController implements Initializable {
@@ -31,6 +31,8 @@ public class FXMLCadastroUserController implements Initializable {
     @FXML
     private Pane cadastroUserScreen;
     @FXML
+    private ListView<String> lvSelecaoPessoa;
+    @FXML
     private PasswordField passFieldConfirmSenha;
     @FXML
     private PasswordField passFieldSenha;
@@ -41,137 +43,77 @@ public class FXMLCadastroUserController implements Initializable {
     @FXML
     private RadioButton rb_operador;
     @FXML
-    private TextField textFieldCPF;
-    @FXML
-    private TextField textFieldEmail;
-    @FXML
-    private TextField textFieldNome;
-    @FXML
-    private TextField textFieldTelefone;
+    private TextField textFieldSelecionaPessoa;
     @FXML
     private TextField textFieldUsername;
 
-    private DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    LinkedList<Pessoa> listPessoas = new LinkedList<>();
+    LinkedList<String> listAuxPessoa = new LinkedList<>();
 
+    ObservableList<String> obsPessoas;
+
+    private Pessoa pessoaEscolhida = new Pessoa();
     private static Usuario usuario = new Usuario();
-    private static PessoaDAO pessoaDAO = new PessoaDAO();
-    private static UsuarioDAO usuarioDAO = new UsuarioDAO();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         CaseTextFormatter.applyLowerCase(textFieldUsername);
-    }
-
-    @FXML
-    void tfCPFKeyReleased(KeyEvent event) {
-        TextFieldFormatter tff = new TextFieldFormatter();
-        tff.setMask("###.###.###-##");
-        tff.setCaracteresValidos("0123456789");
-        tff.setTf(textFieldCPF);
-        tff.formatter();
-    }
-
-    @FXML
-    void textFieldTelefoneOnKeyReleased(KeyEvent event) {
-        TextFieldFormatter tff = new TextFieldFormatter();
-        tff.setMask("(##)#####-####");
-        tff.setCaracteresValidos("0123456789");
-        tff.setTf(textFieldTelefone);
-        tff.formatter();
+        carregarDados();
     }
 
     @FXML
     void btnCadastrarMouseClicked(MouseEvent event) {
-
+        lvSelecaoPessoa.setVisible(false);
         // Verifico se todos os campos estão preenchidos
         if(
-                !textFieldNome.getText().isEmpty() &&
-                        !textFieldEmail.getText().isEmpty() &&
-                        !textFieldUsername.getText().isEmpty() &&
-                        !textFieldTelefone.getText().isEmpty() &&
-                        !textFieldCPF.getText().isEmpty() &&
-                        !passFieldSenha.getText().isEmpty() &&
-                        !passFieldConfirmSenha.getText().isEmpty() &&
-                        (rb_admin.isSelected() || rb_gerente.isSelected() || rb_operador.isSelected())
+                !textFieldSelecionaPessoa.getText().isEmpty() &&
+                !textFieldUsername.getText().isEmpty() &&
+                !passFieldSenha.getText().isEmpty() &&
+                !passFieldConfirmSenha.getText().isEmpty() &&
+                (rb_admin.isSelected() || rb_gerente.isSelected() || rb_operador.isSelected())
         ) {
             // Verifica se o campo senha e comfimação de senha são iguais
             if(passFieldSenha.getText().equals(passFieldConfirmSenha.getText())) {
 
-                // Valida o e-mail digitado para saber se é um e-mail válido
-                if(EmailValidator.isValidEmail(textFieldEmail.getText())) {
+                // Verifica se já existe alguma pessoa usuária cadastrada com esse username no banco de dados
+                if (new VerifyDAO().verifyUsername(textFieldUsername.getText())) {
 
-                    // Verificar se o email já foi cadastrado no banco de dados
-                    if(!VerifyDAO.verifyEmail(textFieldEmail.getText())) {
+                    // Pergunto se a pessoa deseja mesmo realizar o cadastro
+                    int opcao = JOptionPane.showOptionDialog(null, "Tem certeza que deseja cadastrar um novo usuário?", "Confirmação final",
+                            JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[]{"Sim", "Não"}, 0);
 
-                        // Valida se o CPF digitado é um CPF válido/real
-                        if (CPFValidator.validateCPF(textFieldCPF.getText())) {
+                    // Se o botão sim for apertado, cadastramos o novo usuário
+                    if (opcao == 0) {
 
-                            // Verifica se já existe alguma pessoa usuária cadastrada com esse CPF no banco de dados
-                            if (new VerifyDAO().verifyCPF(textFieldCPF.getText())) {
+                        usuario.setIdUsuario(pessoaEscolhida.getIdPessoa());
+                        usuario.setUsername(textFieldUsername.getText());
+                        usuario.setPassword(passFieldSenha.getText());
+                        if (rb_operador.isSelected())
+                            usuario.setRole("Operador");
+                        else if (rb_gerente.isSelected())
+                            usuario.setRole("Gerente");
+                        else if (rb_admin.isSelected())
+                            usuario.setRole("Administrador");
 
-                                // Verifica se já existe alguma pessoa usuária cadastrada com esse username no banco de dados
-                                if (new VerifyDAO().verifyUsername(textFieldUsername.getText())) {
+                        UsuarioDAO.save(usuario);
 
-                                    // Pergunto se a pessoa deseja mesmo realizar o cadastro
-                                    int opcao = JOptionPane.showOptionDialog(null, "Tem certeza que deseja cadastrar um novo usuário?", "Confirmação final",
-                                            JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[]{"Sim", "Não"}, 0);
-
-                                    // Se o botão sim for apertado, cadastramos o novo usuário
-                                    if (opcao == 0) {
-                                        usuario.setNome(textFieldNome.getText());
-                                        usuario.setCPF(textFieldCPF.getText());
-                                        usuario.setEmail(textFieldEmail.getText());
-                                        usuario.setTelefone(textFieldTelefone.getText());
-
-                                        usuario.setUsername(textFieldUsername.getText());
-                                        usuario.setPassword(passFieldSenha.getText());
-                                        if (rb_operador.isSelected())
-                                            usuario.setRole("Operador");
-                                        else if (rb_gerente.isSelected())
-                                            usuario.setRole("Gerente");
-                                        else if (rb_admin.isSelected())
-                                            usuario.setRole("Administrador");
-
-                                        pessoaDAO.save(usuario);
-
-                                        usuarioDAO.save(usuario);
-
-                                        textFieldNome.setText(null);
-                                        textFieldUsername.setText(null);
-                                        textFieldCPF.setText(null);
-                                        textFieldEmail.setText(null);
-                                        textFieldTelefone.setText(null);
-                                        passFieldSenha.setText(null);
-                                        passFieldConfirmSenha.setText(null);
-                                    }
-                                    else {
-                                        JOptionPane.showMessageDialog(null, "O usuário NÃO foi cadastrado!",
-                                                "Cancelamento do cadastro do usuário", JOptionPane.INFORMATION_MESSAGE);
-                                        btnCancelarMouseClicked(null);
-                                    }
-                                }
-                                else {
-                                    JOptionPane.showMessageDialog(null, "Já existe um usuário com esse username cadastrado\n Digite um username diferente, por favor",
-                                            "Erro tentar realizar cadastro", JOptionPane.WARNING_MESSAGE);
-                                }
-                            }
-                            else {
-                                JOptionPane.showMessageDialog(null, "Esse CPF já foi cadastrado em algum usuário!",
-                                        "Erro tentar realizar cadastro", JOptionPane.WARNING_MESSAGE);
-                            }
-                        }
-                        else {
-                            JOptionPane.showMessageDialog(null, "O CPF digitado é inválido\nPor favor, digite um CPF válido/correto!",
-                                    "Erro tentar realizar cadastro", JOptionPane.WARNING_MESSAGE);
+                        if(UsuarioDAO.getResult())
+                        {
+                            textFieldSelecionaPessoa.setText(null);
+                            textFieldUsername.setText(null);
+                            passFieldSenha.setText(null);
+                            passFieldConfirmSenha.setText(null);
+                            UsuarioDAO.setDefaultResult();
                         }
                     }
                     else {
-                        JOptionPane.showMessageDialog(null, "O E-mail digitado já está em uso\nPor favor, digite tente com outro E-mail",
-                                "Erro tentar realizar cadastro", JOptionPane.WARNING_MESSAGE);
+                        JOptionPane.showMessageDialog(null, "O usuário NÃO foi cadastrado!",
+                                "Cancelamento do cadastro do usuário", JOptionPane.INFORMATION_MESSAGE);
+                        btnCancelarMouseClicked(null);
                     }
                 }
                 else {
-                    JOptionPane.showMessageDialog(null, "O E-mail digitado é inválido\nPor favor, digite um E-mail válido/correto!",
+                    JOptionPane.showMessageDialog(null, "Já existe um usuário com esse username cadastrado\n Digite um username diferente, por favor",
                             "Erro tentar realizar cadastro", JOptionPane.WARNING_MESSAGE);
                 }
             }
@@ -190,12 +132,79 @@ public class FXMLCadastroUserController implements Initializable {
 
     @FXML
     void btnCancelarMouseClicked(MouseEvent event) {
-        textFieldNome.setText(null);
+        textFieldSelecionaPessoa.setText(null);
         textFieldUsername.setText(null);
-        textFieldCPF.setText(null);
-        textFieldEmail.setText(null);
-        textFieldTelefone.setText(null);
         passFieldSenha.setText(null);
         passFieldConfirmSenha.setText(null);
+        rb_admin.setSelected(false);
+        rb_gerente.setSelected(false);
+        rb_operador.setSelected(false);
+    }
+
+    @FXML
+    void pfConfirmSenhaOnMouseClicked(MouseEvent event) {
+        lvSelecaoPessoa.setVisible(false);
+    }
+
+    @FXML
+    void pfSenhaOnMouseClicked(MouseEvent event) {
+        lvSelecaoPessoa.setVisible(false);
+    }
+
+    @FXML
+    void tfSelecionePessoaOnMouseClicked(MouseEvent event) {
+        lvSelecaoPessoa.setVisible(true);
+    }
+
+    @FXML
+    void tfUsernameOnMouseClicked(MouseEvent event) {
+        lvSelecaoPessoa.setVisible(false);
+    }
+
+    private void carregarDados() {
+        // Listas principais recebem os dados do banco
+        listPessoas = PessoaDAO.getPessoa();
+
+        // Passando alguns dados específicos para listas auxiliares que exibirão os dados na ListView
+        for(Pessoa p : listPessoas)
+            listAuxPessoa.add(p.getNome()+" - "+p.getCPF().substring(0, 3)+".***.***-"+p.getCPF().substring(12, 14)); // formatando a exibição dos dados: Nome da pessoa + CPF: 000.***.***-00
+
+        // Carregamos as listas auxiliares com os dados exibíveis
+        obsPessoas = FXCollections.observableArrayList(listAuxPessoa);
+
+        // FilteredLists para filtragem dinâmica
+        FilteredList<String> filteredPessoas = new FilteredList<>(obsPessoas, p -> true);
+
+        // Associa as listas filtradas às ListViews
+        lvSelecaoPessoa.setItems(filteredPessoas);
+
+        // Listener para o filtro dinâmico conforme o usuário digita no TextField
+        textFieldSelecionaPessoa.textProperty().addListener((obs, oldVal, newVal) -> {
+            filteredPessoas.setPredicate(item -> {
+                if (newVal == null || newVal.isEmpty()) return true;
+                String lower = newVal.toLowerCase();
+                return item.toLowerCase().contains(lower);
+            });
+        });
+
+        // Preenchendo o TextField ao clicar em um item da ListView
+        lvSelecaoPessoa.setOnMouseClicked(e -> {
+            String selected = lvSelecaoPessoa.getSelectionModel().getSelectedItem();
+
+            for(Pessoa p : listPessoas) // Procurando o item na lista, se for encontrado, armazenamos os dados da pessoa escolhida
+            {
+                if((p.getNome()+" - "+p.getCPF().substring(0, 3)+".***.***-"+p.getCPF().substring(12, 14)).equals(selected)) // Comparando a formatação do item da vez com o item selecionado
+                {
+                    pessoaEscolhida = p;
+                    break;
+                }
+            }
+
+            if (selected != null) {
+                textFieldSelecionaPessoa.setText(selected);
+                lvSelecaoPessoa.setVisible(false);
+            }
+        });
+
     }
 }
