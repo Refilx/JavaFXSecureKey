379b01673d5fd8ce9ac642790e1be33b0dc60f98
Refilx <jovenestudantex11@gmail.com>
2025-04-23 18:26:30 -0300

    Criando a tela de FXMLChavesCadastradasScreen e a classe controladora com a estrutura básica FXMLChavesCadastradasController

---
+++ src/main/java/br/com/javafxsecurekey/controller/FXMLChavesCadastradasController.java
@@ -0,0 +1,33 @@
+package br.com.javafxsecurekey.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class FXMLChavesCadastradasController {
+
+    @FXML
+    private TableColumn<?, ?> tc_bloco;
+
+    @FXML
+    private TableColumn<?, ?> tc_nomeSala;
+
+    @FXML
+    private TableColumn<?, ?> tc_numChave;
+
+    @FXML
+    private TableColumn<?, ?> tc_observacoes;
+
+    @FXML
+    private TableColumn<?, ?> tc_quantidade;
+
+    @FXML
+    private TableColumn<?, ?> tc_status;
+
+    @FXML
+    private TableColumn<?, ?> tc_temReserva;
+
+    @FXML
+    private TableView<?> tvHistLogin;
+
+}
